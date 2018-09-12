6c3987b0a1bec6fb98c7c0b0f3abb6f46ed3f143
Andrew Matthews <ajmatthews845@gmail.com>
2018-09-12 20:23:28 +0100

    First commit

---
+++ app/src/main/java/com/mobile/andrew/dissertationtest/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.mobile.andrew.dissertationtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
